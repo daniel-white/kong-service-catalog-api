c730f59c520c85f04a7be4e71ae9fbbd5622e383
Daniel A. White <daniel@whitefamily.in>
2025-05-15 13:05:38 -0400

    cleanup

--- src/module.ts
+++ src/module.ts
@@ -1,21 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { DatabaseModule } from './database/module';
 import { TenantsModule } from './tenants/module';
 import { AuthModule } from './auth/module';
 import { ZodValidationPipe } from 'nestjs-zod';
 import { APP_PIPE } from '@nestjs/core';
 
 @Module({
-  imports: [AuthModule, DatabaseModule, TenantsModule],
-  controllers: [AppController],
+  imports: [AuthModule, TenantsModule],
+  controllers: [],
   providers: [
     {
       provide: APP_PIPE,
       useClass: ZodValidationPipe,
     },
-    AppService,
   ],
 })
 export class AppModule {}
